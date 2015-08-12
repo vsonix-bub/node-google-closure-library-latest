c36484d749db971f3c1f35085c84a23fd4bf3c01
Valeriy Yatsko <dwr@design.ru>
2015-08-12 14:46:16 +0300

    Fix install.js

--- install.js
+++ install.js
@@ -6,7 +6,7 @@ var download = require('download');
 var closureToolsUrls = 'https://github.com/google/closure-library/archive/master.zip';
 var downloadDir = path.join(__dirname, 'lib');
 
-var closureToolsDownload = download({ extract: true , strip: 1})
+var closureToolsDownload = download({ mode: 755, extract: true , strip: 1})
 	.get(closureToolsUrls)
 	.dest(downloadDir);
 
